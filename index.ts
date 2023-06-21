52c95d58b9ddb09757e8d49b3ff07faa1a43d539
Lucas De Curtis <lucas.de-curtis@epitech.eu>
2023-06-21 20:29:21 +0900

    first commit
    Signed-off-by: Lucas De Curtis <lucas.de-curtis@epitech.eu>

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const phoneSchema = z
+  .string()
+  .max(50)
+  .transform((value) => {
+    if (!value) return value;
+
+    const currentValue = value.replace(/\s/g, "");
+
+    if (currentValue.startsWith("+33")) {
+      return currentValue.slice(3);
+    }
+    if (currentValue.startsWith("0033")) {
+      return currentValue.slice(4);
+    }
+    if (currentValue.startsWith("0")) {
+      return currentValue.slice(1);
+    }
+    return currentValue;
+  });
+
+export { z };
